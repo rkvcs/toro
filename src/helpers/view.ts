5feedbc30b26357fe61421628d040105e81fc901
Ricardo <ricardo.kovacs@gmail.com>
2025-07-13 23:17:28 -0300

    update default layout

--- src/helpers/view.ts
+++ src/helpers/view.ts
@@ -3,5 +3,5 @@ import { Eta } from "eta"
 const eta = new Eta({ views: "./resources/view/" })
 
 export function view(file: string, data: Object){
-	return eta.render(`${file}.eta.html`, data)
+	return eta.render(`${file}.eta`, data)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { Hono } from "hono";
 import { serveStatic } from 'hono/deno'
 import { config, view, loadPackages } from "@app/helpers/index.ts";
+import {
+  matchedRoutes,
+  routePath,
+  baseRoutePath,
+  basePath,
+} from 'hono/route'
+
 
 const app = new Hono();
 
@@ -9,6 +16,10 @@ app.use('*', serveStatic({ "root": "./public" }))
 loadPackages(app)
 
 app.get("/", (c) => {
+	const routes = matchedRoutes(c)
+
+	console.info(routes)
+
 	return c.html(view('pages/home', {
 		name: config('APP_NAME')
 	}))
